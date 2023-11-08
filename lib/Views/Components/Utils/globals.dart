0bb6d7023c0403575db4033ef489bfee6c145773
r37 <vaibhavbhalala1212@gmail.com>
2023-11-08 16:52:01 +0530

    add category icons

---
+++ lib/Views/Components/Utils/globals.dart
@@ -0,0 +1,17 @@
+List<String> CategoryIcons = [
+  "lib/Views/Components/Utils/category_Icons/bill.png",
+  "lib/Views/Components/Utils/category_Icons/cash.png",
+  "lib/Views/Components/Utils/category_Icons/communication.png",
+  "lib/Views/Components/Utils/category_Icons/deposit.png",
+  "lib/Views/Components/Utils/category_Icons/food.png",
+  "lib/Views/Components/Utils/category_Icons/gift.png",
+  "lib/Views/Components/Utils/category_Icons/health.png",
+  "lib/Views/Components/Utils/category_Icons/movie.png",
+  "lib/Views/Components/Utils/category_Icons/other.png",
+  "lib/Views/Components/Utils/category_Icons/rupee.png",
+  "lib/Views/Components/Utils/category_Icons/salary.png",
+  "lib/Views/Components/Utils/category_Icons/shopping.png",
+  "lib/Views/Components/Utils/category_Icons/transport.png",
+  "lib/Views/Components/Utils/category_Icons/wallet.png",
+  "lib/Views/Components/Utils/category_Icons/withdraw.png",
+];
